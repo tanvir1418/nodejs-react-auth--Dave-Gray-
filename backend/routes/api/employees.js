9f075528ae3906fc562844d260d01678b0500393
Tanvir <tanvirahmed1418@gmail.com>
2022-10-02 22:17:09 +0600

    MVC REST API : Employee

--- backend/routes/api/employees.js
+++ backend/routes/api/employees.js
@@ -1,31 +1,14 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const data = {};
-data.employees = require("../../model/employees.json");
+const employeesController = require('../../controllers/employeesController');
 
 router
-  .route("/")
-  .get((req, res) => {
-    res.json(data.employees);
-  })
-  .post((req, res) => {
-    res.json({
-      firstname: req.body.firstname,
-      lastname: req.body.lastname,
-    });
-  })
-  .put((req, res) => {
-    res.json({
-      firstname: req.body.firstname,
-      lastname: req.body.lastname,
-    });
-  })
-  .delete((req, res) => {
-    res.json({ id: req.body.id });
-  });
+  .route('/')
+  .get(employeesController.getAllEmployees)
+  .post(employeesController.createNewEmployee)
+  .put(employeesController.updateEmployee)
+  .delete(employeesController.deleteEmployee);
 
-router.route("/:id").get((req, res) => {
-  res.json({ id: req.params.id });
-});
+router.route('/:id').get(employeesController.getEmployee);
 
 module.exports = router;
