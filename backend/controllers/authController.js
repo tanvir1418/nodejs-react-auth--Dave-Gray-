e9399f5b730442b8dbbfc0a5972799d9782f9b61
Tanvir <tanvirahmed1418@gmail.com>
2022-10-07 22:26:29 +0600

    Create Model and Connect to MongoDB

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,24 +1,15 @@
-const usersDB = {
-    users: require('../model/users.json'),
-    setUsers: function (data) {
-        this.users = data;
-    },
-};
-
+const User = require('../model/User');
 const bcrypt = require('bcrypt');
-
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
-const fsPromises = require('fs').promises;
-const path = require('path');
 
 const handleLogin = async (req, res) => {
     const { user, pwd } = req.body;
     if (!user || !pwd)
         return res.status(400).json({ message: 'Username and password are required.' });
 
-    const foundUser = usersDB.users.find((person) => person.username === user);
+    const foundUser = await User.findOne({ username: user }).exec();
     if (!foundUser) return res.sendStatus(401); // Unauthorized
+
     // Evaluate password
     const match = await bcrypt.compare(pwd, foundUser.password);
     if (match) {
@@ -39,13 +30,11 @@ const handleLogin = async (req, res) => {
                 expiresIn: '1d',
             }
         );
-        const otherUsers = usersDB.users.filter((person) => person.username !== foundUser.username);
-        const currentUser = { ...foundUser, refreshToken };
-        usersDB.setUsers([...otherUsers, currentUser]);
-        await fsPromises.writeFile(
-            path.join(__dirname, '..', 'model', 'users.json'),
-            JSON.stringify(usersDB.users)
-        );
+
+        // Saving refreshToken with current user
+        foundUser.refreshToken = refreshToken;
+        const result = await foundUser.save();
+        console.log(result);
 
         //==================================================
         // secure: true is removed because Thunder Client can't access the cookies
